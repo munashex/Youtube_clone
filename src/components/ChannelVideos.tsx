9b4f41b0071e91aab72dcd172e8a84a05d50c4b9
munashex <munashemugondaa@gmail.com>
2024-09-03 04:27:49 +0200

    fixing image size

--- src/components/ChannelVideos.tsx
+++ src/components/ChannelVideos.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { VideoTypes } from '../types/Video'; 
 import { Link } from 'react-router-dom';
 
+
 interface VideoProps {
   video: VideoTypes;
 }
@@ -15,36 +16,25 @@ const formatViewCount = (viewCount: string): string => {
 };
 
 const ChannelVideos: React.FC<VideoProps> = ({ video }) => {
-  const thumbnailUrl = video.thumbnail?.[1]?.url || video.thumbnail?.[0]?.url;
+  const thumbnailUrl = video.thumbnail?.[2]?.url || video.thumbnail?.[0]?.url;
+  
 
   return (
-    <div className="flex flex-col">
-      <Link to={`/video/${video.videoId}`} className="relative">
-        <img 
-          src={thumbnailUrl} 
-          alt={video.title} 
-          className="w-full aspect-video object-cover rounded-xl"
-        />
-        <span className="absolute bottom-1 right-1 bg-black text-white text-xs font-semibold px-1 py-0.5 rounded">
-          {video.lengthText}
-        </span>
-      </Link>
-      <div className="mt-2">
-        <div className="flex flex-col">
-          <h3 className="font-semibold line-clamp-2 leading-5">
-            {video.title}
-          </h3>
-          <Link to={`/channel/${video.channelId}`} className="text-gray-600 mt-1">
-            {video.channelTitle}
-          </Link>
-          <p className="text-gray-600">
-            {formatViewCount(video.viewCount)} views • {video.publishedTimeText}
-          </p>
-        </div>
-      </div>
+  <Link to={`/video/${video.videoId}`} className="flex flex-col gap-x-2">
+    <img src={thumbnailUrl} alt={video.title} 
+    className="w-full h-56 rounded-md"
+    />
+    <div className="flex flex-col gap-x-1"> 
+      <h1 className="font-bold">{video.title.length < 58 ? video.title : video.title.slice(0, 58) + '....'}</h1> 
+      <p className=" text-gray-500 inline-flex items-center gap-x-2">
+        <span>{formatViewCount(video.viewCount)}  views • </span> 
+        <span>{video.publishedTimeText}</span>
+      </p>
     </div>
+  </Link>
   );
 };
 
 export default ChannelVideos;
+
 
